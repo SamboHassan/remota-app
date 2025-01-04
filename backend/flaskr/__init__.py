72796524aacda814f706d33ecde2b2551555a6b4
SamboHassan <sambohasan@gmail.com>
2025-01-04 09:43:36 +0100

    update: add error handling to register endpoint

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -1,10 +1,11 @@
-from flask import Flask, jsonify, abort, request
+# ----------------------------------------------------------------------------#
+# Imports
+# ----------------------------------------------------------------------------#
 
+from flask import Flask, jsonify, abort, request, url_for, flash, redirect
 from werkzeug.http import HTTP_STATUS_CODES
-from flask_jwt_extended import jwt_required
+from flask_jwt_extended import jwt_required, create_access_token, create_refresh_token
 from passlib.hash import pbkdf2_sha256
-
-
 from models import db, UserModel, JobModel
 
 
@@ -18,23 +19,48 @@ def create_app():
     # -------- Endpoints ---------#
 
     @app.route("/")
-    def home():
-        return jsonify({"success": True, "message": "Hello world placeholder"})
+    def index():
+        try:
+            users = UserModel.query.all().sort_by(UserModel.username)
+            return jsonify({"users": users})
+        except Exception as e:
+            print(f'Exception "{e}"')
+            flash("An Error had occurred, users could not be fetched")
 
     @app.route("/users", methods=["POST"])
     def register():
-        user_data = request.get_json()
-        if UserModel.query.filter(UserModel.username == user_data["username"]).first():
-            abort(409)
+        insert_error = False
+        try:
+            user_data = request.get_json()
+            if UserModel.query.filter(
+                UserModel.username == user_data["username"]
+            ).first():
+                abort(409)
+            user = UserModel(
+                username=user_data["username"],
+                password=pbkdf2_sha256.hash(user_data["password"]),
+                email=user_data["email"],
+                is_admin=user_data["is_admin"],
+            )
+            db.session.add(user)
+            db.session.commit()
 
-        user = UserModel(
-            username=user_data["username"],
-            password=pbkdf2_sha256.hash(user_data["password"]),
-            email=user_data["email"],
-        )
-        db.session.add(user)
-        db.session.commit()
-        return jsonify({"id": user.id, "password": user.password})
+        except Exception as e:
+            insert_error = True
+            print(f'Exception "{e}"')
+            db.session.rollback()
+
+        finally:
+            db.session.close()
+
+        if not insert_error:
+            flash("You are successfully registred!")
+            return user.format()
+            # return redirect(url_for("index"))
+        else:
+            flash("could not be listed.")
+            abort(500)
+            return redirect(url_for("index"))
 
     with app.app_context():
         db.create_all()
