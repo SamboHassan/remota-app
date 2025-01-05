772b84259ad04bad365d0c86d0ac54649b6b98b0
SamboHassan <sambohasan@gmail.com>
2025-01-05 19:12:44 +0100

    add login endpoint

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,19 +1,11 @@
 from db import db
 from datetime import datetime
 import json
-
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
-from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy.sql import func
 
-"""
-User model
 
-"""
-
-
-class UserModel(db.Model, UserMixin):
+class UserModel(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
     username = db.Column(db.String(150), unique=True, nullable=False)
@@ -21,17 +13,3 @@ class UserModel(db.Model, UserMixin):
     email = db.Column(db.String(200), unique=True, nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, server_default=func.now())
     is_admin = db.Column(db.Boolean, default=False)
-
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
-
-    def format(self):
-        return {
-            "id": self.id,
-            "username": self.username,
-            "email": self.email,
-            "is_admin": self.is_admin,
-        }
