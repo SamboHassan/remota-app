b3a8415f93506bd3aad9e4ef5bef4ea72c3e794d
SamboHassan <sambohasan@gmail.com>
2025-02-14 16:44:47 +0100

    feat: add chakra ui

--- remota/.next/server/server-reference-manifest.js
+++ remota/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"HsaxG6lAyyRaXLDj9GcIjII7vHFRQdBJTt7TFgkllfc=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
